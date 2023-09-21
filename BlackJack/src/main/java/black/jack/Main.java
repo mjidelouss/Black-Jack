1b5939d1ea4f4412f8770fae54f2dabff731cff6
mjidelouss <mjid.elouss@gmail.com>
2023-09-21 13:33:00 +0100

    Create deck using double dimensienal array (Basics) + Creating function constructDeckFromCard that takes two parameters cardNumber and suit

--- BlackJack/src/main/java/black/jack/Main.java
+++ BlackJack/src/main/java/black/jack/Main.java
@@ -9,19 +9,71 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+        int [][] deck = creatDeckCards();
+        int [][] newDeck = constructDeckFromCard(8,4);
+        for (int i = 0; i < deck.length; i++) {
+            System.out.print("(" + deck[i][0] + " " + deck[i][1] + ")");
+        }
+        System.out.print("\n\n");
+        for (int i = 0; i < newDeck.length; i++) {
+            System.out.print("(" + newDeck[i][0] + " " + newDeck[i][1] + ")");
+        }
     }
 
-    public static List creatDeckCards() {
-        List<Map<Integer, Integer>> deck = new ArrayList<>();
-
-        for (int i = 1; i <= 4; i++) {
+    public static int[][] creatDeckCards() {
+        int [][] deck = new int [52][2];
+        int index = 0;
+        for (int i = 1; i <= 4 ; i++) {
             for (int j = 1; j <= 13; j++) {
-                Map<Integer, Integer> hashMap = new HashMap<>();
-                hashMap.put(j, i);
-                deck.add(hashMap);
+                deck[index][0] = j;
+                deck[index][1] = i;
+                index++;
             }
         }
         return deck;
+    }
+
+    public static int[][] constructDeckFromCard(int cardNumber, int suit) {
+        int size = (13 - cardNumber + 1) + (4 - suit) * 13;
+        int [][] newDeck = new int [size][2];
+        int index = 0;
+        for (int i = suit; i <= 4 ; i++) {
+            for (int j = cardNumber; j <= 13; j++) {
+                newDeck[index][0] = j;
+                newDeck[index][1] = i;
+                index++;
+            }
+        }
+        return newDeck;
+    }
+
+    public static void art() {
+        System.out.print("\n\n");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                            ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀  ⠀            ⠀⠀⠀  ⠀     ⠀⠀⠀  ⠀     ⠀⠀⠀  ⠀     ⠀⠀⠀  ⠀     ⠀⠀⠀    ⠀⠀⠀⠀⠀ ⣀⣤⣴⣄⠀⢀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                         ⠀     ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀            ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀     ⠀⠀⠀  ⣠⣴⣾⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                                ███████████  ████                     █████               █████                    █████⠀ ⠀⢿⣿⣿⣿⠛⠿⣿⣿⣿⡀⢻⣿⣿⣿⣿⠀⣸⣿⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                              ░░███░░░░░███░░███                    ░░███               ░░███                    ░░███   ⠀ ⠀⠘⣿⣿⠃⠀⠀⠀⠈⠙⣧⠈⢿⣿⣿⣿⠀⣿⣿⣿⣿⡟⢀⡀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                               ░███    ░███ ░███   ██████    ██████  ░███ █████          ░███   ██████    ██████  ░███ █████⠀⠀⢹⡇⠀⠀⠀⠀⣀⣠⣿⣇⠘⣿⣿⣿⠀⣿⣿⣿⡿⠀⣾⣿⣷⣄⠀⠀⠀\n" +
+                '\t'+"\t\t                               ░██████████  ░███  ░░░░░███  ███░░███ ░███░░███           ░███  ░░░░░███  ███░░███ ░███░░███  ⠀⠀⠀⢿⣦⣤⣾⡆⣹⣿⣿⣿⡄⠹⣿⣿⠀⣿⣿⣿⠃⣸⣿⣿⣿⣿⣷⠀⠀\n" +
+                '\t'+"\t\t                               ░███░░░░░███ ░███   ███████ ░███ ░░░  ░██████░            ░███   ███████ ░███ ░░░  ░██████░ ⠀ ⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⠗⢀⣿⡏⠀⣿⣿⡏⢠⣿⣿⣿⣿⠟⠁⠀⠀\n" +
+                '\t'+"\t\t                               ░███░░░░░███ ░███   ███████ ░███ ░░░  ░██████░            ░███   ███████ ░███ ░░░  ░██████░⠀⠀  ⠀⠀⠀⠸⢿⠿⠟⠋⠉⠁⠀⠐⠚⠛⠃⣰⣿⡿⠀⣾⣿⣿⡿⠃⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                               ░███    ░███ ░███  ███░░███ ░███  ███ ░███░░███     ███   ░███  ███░░███ ░███  ███ ░███░░███ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠻⠿⠿⠃⣸⣿⣿⠋⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                                ███████████  █████░░████████░░██████  ████ █████   ░░████████  ░░████████░░██████  ████ █████⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠢⣤⣾⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀\n" +
+                '\t'+"\t\t                               ░░░░░░░░░░░  ░░░░░  ░░░░░░░░  ░░░░░░  ░░░░ ░░░░░     ░░░░░░░░    ░░░░░░░░  ░░░░░░  ░░░░ ░░░░░⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    }
+    public static void gameMenu() {
+        System.out.print("\n\n\n");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ╔═░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░  ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░░ ░░ ░░ ░░═╗");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                                                                     ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                      BLACK JACK                                     ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                        HALALAL                                      ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                                                                     ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                                                                     ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░ (1) - Start Game.                                                                   ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░ (2) - Quit.                                                                         ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ░░                                                                                     ░░");
+        System.out.println('\t'+ Colors.RESET.getColor() +"\t                                            ╚═░░ ░░ ░░ ░░ ░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░═╝");
     }
 }
